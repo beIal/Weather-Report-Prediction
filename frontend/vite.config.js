4aa399869d3c983a01981b8bf12f378c7542391e
Joseph Linao <linaojoseph3111@gmail.com>
2024-10-31 14:04:10 +1100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/,  // Apply only to .js files in the src directory
+  server: {
+    port: 3000,  // Or any other available port
   },
 });
+
 

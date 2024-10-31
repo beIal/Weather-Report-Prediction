82a6f2ff03bebe6effb824890ae4528abf344274
Joseph Linao <linaojoseph3111@gmail.com>
2024-10-31 13:38:23 +1100

    chicken

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,10 +1,14 @@
 import pickle 
 
 def load_model(): 
-    classifcation_model_path = r"C:\Users\cucum\Downloads\COS30049\assignment 2\Classification.py" 
-    regression_model_path = r"C:\Users\cucum\Downloads\COS30049\assignment 2\Regression.py"  
+    # Updates paths to find point to the picke files 
+    classifcation_model_path = r"C:\Users\cucum\Downloads\COS30049\assignment 2\classification_model.pkl" 
+    regression_model_path = r"C:\Users\cucum\Downloads\COS30049\assignment 2\regression_model.pkl"   
+    
+    # Load Models
     classification_model = pickle.load(open(classifcation_model_path, 'rb')) 
-    regression_model = pickle.load(open(regression_model_path, 'rb')) 
+    regression_model = pickle.load(open(regression_model_path, 'rb'))  
+    
     return classification_model, regression_model 
 
 def predict(model, data, model_type): 

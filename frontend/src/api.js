fe2d549a6fa8acc6de975d512f7d196ff2a37903
Joseph Linao <linaojoseph3111@gmail.com>
2024-11-01 12:28:03 +1100

    m

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'; 
 
-const APP_URL = 'http://localhost:8000'; 
+const API_URL = 'http://localhost:8000'; 
 
 export const classifyWeather = async (inputData) => { 
     try { 

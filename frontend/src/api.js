8a05aaa31eee259fd6ff3ab2df882bd92423d334
Joseph Linao <linaojoseph3111@gmail.com>
2024-11-01 12:30:40 +1100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,21 @@
-import axios from 'axios'; 
+import axios from 'axios';
 
-const API_URL = 'http://localhost:8000'; 
+const API_URL = 'http://localhost:8000';
 
-export const classifyWeather = async (inputData) => { 
-    try { 
-        const response = await axios.post('${API_URL}/predict/classification', inputData); 
+export const classifyWeather = async (inputData) => {
+    try {
+        const response = await axios.post(`${API_URL}/predict/classification`, inputData); // Use backticks here
         return response.data.prediction;
-    } catch (error) { 
+    } catch (error) {
         throw error.response ? error.response.data.detail : 'Network error';
     }
-}; 
+};
 
-export const regressWeather = async (inputData) => { 
-    try { 
-        const response = await axios.post('${API_URL}/predict/regression', inputData); 
+export const regressWeather = async (inputData) => {
+    try {
+        const response = await axios.post(`${API_URL}/predict/regression`, inputData); // Use backticks here
         return response.data.prediction;
-    } catch (error) { 
+    } catch (error) {
         throw error.response ? error.response.data.detail : 'Network error';
     }
 };

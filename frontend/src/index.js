196032e80d4064b5b6fda0d6a551fed2f303f133
Joseph Linao <linaojoseph3111@gmail.com>
2024-11-01 14:11:35 +1100

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from './react';  
+import ReactDom from 'react-dom'; 
+import App from './App'; 
+
+ReactDom.render( 
+    <react.StrictMode> 
+        <App/>
+    </react.StrictMode>, 
+    document.getElementsById('root')
+);
